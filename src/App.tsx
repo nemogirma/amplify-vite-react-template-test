b2d2abaeea3d9fe02d2fe6b83cc0ca43dcf49b79
nterefe <79771767+nterefe@users.noreply.github.com>
2024-11-02 17:24:24 -0700

    draft website fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import MainLayout from "../amplify/components/MainLayout";
+import MainLayout from "./components/MainLayout";
 
 function App() {
   return (
